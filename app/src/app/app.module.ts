51538a3f1dd473fa90edfb3f1e6edc0930bb9ce7
Przemyslaw Wisniewski <przemyslaw.wisniewski@algoteque.com>
2020-04-07 22:37:31 +0200

    dif folder structure

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProgresBarComponent } from './components/progres-bar/progres-bar.component';
-import { TestComponent } from './components/test/test/test.component';
+import { TestComponent } from './components/test/test.component';
 
 @NgModule({
   declarations: [AppComponent, ProgresBarComponent, TestComponent],
